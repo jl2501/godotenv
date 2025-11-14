5ae667398e5cad3e98c7d61453e7b988935936e3
James Light <jl25019@gmail.com>
2025-11-14 18:36:50 -0500

    Use a minimal Filesystem interface
    - afero.Fs has a lot of methods we don't actually use - this way its
      easier to sub afero w/ another fs lib

---
+++ fs.go
@@ -0,0 +1,26 @@
+package godotenv
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// minimum set of methods needed for a File type
+type Filer interface {
+	io.Reader
+	io.Closer
+	WriteString(string) (int, error)
+	Sync() error
+	Stat() (os.FileInfo, error)
+}
+
+// minimum set of methods needed to pass a filesytem type
+// at least can be afero or billy - hopefully unknown others as well
+type Filesyser interface {
+	Open(string) (Filer, error)
+	Create(string) (Filer, error)
+	MkdirAll(string, os.FileMode) error
+	Walk(root string, walkFn filepath.WalkFunc) error
+	WriteFile(string, []byte, os.FileMode) error
+}
